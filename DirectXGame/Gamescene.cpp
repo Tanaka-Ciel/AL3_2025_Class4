96edff17b4676a98c986a4fc64e4b1e17638e684
田中志得 <k022g0070@g.neec.ac.jp>
2025-04-16 18:40:58 +0900

    02_01

--- DirectXGame/Gamescene.cpp
+++ DirectXGame/Gamescene.cpp
@@ -2,7 +2,22 @@
 
 using namespace KamataEngine;
 
-void Gamescene::Initialize() {
+// デストラクタ
+Gamescene::~Gamescene() { 
+	delete model_;
+}
+
+void Gamescene::Initialize() { 
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario_PNG125.png"); 
+
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//カメラの初期化
+	camera_.Initialize();
 
 }
 
